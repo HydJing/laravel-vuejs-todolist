6bf83c26ec6ce4e69ca5c28fd40ec53e725b76fd
Wei Jing <jaychou123103@hotmail.com>
2021-04-04 02:28:39 +1300

    add list view

--- resources/js/vue/listView.vue
+++ resources/js/vue/listView.vue
@@ -1,11 +1,50 @@
 <template>
     <div>
-        List View
+        <div v-for="(item, index) in items" :key="index">
+            <list-item 
+                :item="item"
+                 class="item" 
+                 v-on:itemchanged="$emit('reloadelist')"
+            />
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import listItem from "./listItem";
+import ListItem from "./listItem.vue";
 
+export default {
+    props: ['items'],
+    components: {
+        listItem
+    },
+    data: function() {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        getList() {
+            axios.get('api/items')
+            .then(response => {
+                this.items = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    },
+    created() {
+
+    }
 }
 </script>
+
+<style scoped>
+.item {
+    background: #e6e6e6;
+    padding: 5px;
+    margin-top: 1px;
+}
+</style>
